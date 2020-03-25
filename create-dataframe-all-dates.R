e5c9d6a7647f372057182302e4409f1dfafeff73
Greg <grousell@gmail.com>
2020-03-25 16:38:37 -0400

    Minor change to dates_all

--- create-dataframe-all-dates.R
+++ create-dataframe-all-dates.R
@@ -1,13 +1,24 @@
 # Create data frame of all dates and Public Health Units
+library(tidyverse)
 
 df <- read_csv("~/Dropbox/COVID-19/Ontario-COVID-19-Research/Data/ontario_corona_cases.csv")
 
 df <- df %>% 
-  mutate (Public_Health_Unit = ifelse (Public_Health_Unit == "RichmondHill" | 
-                                         Public_Health_Unit == "Newmarket", "York", Public_Health_Unit )) %>% 
-  mutate (Public_Health_Unit = ifelse(Public_Health_Unit == "Haliburton Kawartha", 
+  mutate (Public_Health_Unit = ifelse (str_detect(Public_Health_Unit, "RichmondHill"), "York",
+                                       ifelse (str_detect(Public_Health_Unit, "Newmarket"), "York",
+                                               Public_Health_Unit))) %>% 
+  mutate (Public_Health_Unit = ifelse(str_detect (Public_Health_Unit, "Haliburton"),
                                       "Haliburton,Kawartha,Pine Ridge",
                                       Public_Health_Unit)) %>% 
+  mutate (Public_Health_Unit = ifelse(str_detect (Public_Health_Unit, "Kingston"),
+                                      "Kingston, Frontenac and Lennox & Addington",
+                                      Public_Health_Unit)) %>% 
+  mutate (Public_Health_Unit = ifelse(str_detect (Public_Health_Unit, "Porcupine"),
+                                      "Porcupine District",
+                                      Public_Health_Unit)) %>% 
+  mutate (Public_Health_Unit = ifelse(str_detect (Public_Health_Unit, "Sudbury"),
+                                      "Sudbury and District",
+                                      Public_Health_Unit)) %>%
   mutate(Public_Health_Unit = ifelse(Public_Health_Unit == "pending" |
                                        Public_Health_Unit == "NotAvailable", "Pending or Not Available", Public_Health_Unit))
 
@@ -30,4 +41,4 @@ remove(list = names)
 write_csv (dates_all, 
            path = "/Users/gregrousell/Dropbox/COVID-19/Ontario-COVID-19-Research/Data/dates_all.csv")
 
-unique(dates_all$Public_Health_Unit)
+
